d06c78f73a23a08f94dc361e736a77faee638c7d
Krzysztof Lasocki <krz.lasocki@gmail.com>
2020-04-13 15:08:24 +0200

    Cwiczenie 3 UART, poprawki makefile żeby uruchamiał program automatycznie po make install (bez przekładania zworek)

---
+++ 02_uart/main.c
@@ -0,0 +1,27 @@
+/*
+ * STM32 - ćwiczenie 1, migająca dioda LED
+ * projekt ,,Python w Elektronicznej Sieci''
+ * Krzysztof Lasocki <krz.lasocki@gmail.com>
+ */
+#ifndef STM32F1
+#define STM32F1
+#endif
+
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+#include <stdio.h>
+
+#include "uart.h"
+int main(){
+  rcc_periph_clock_enable(RCC_GPIOC);
+  gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
+		GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+  usart_setup();
+  while(1){
+    for (int i = 0; i < 150000; i++) __asm__("nop");
+    gpio_toggle(GPIOC, GPIO13);
+    printf("Hello, World!\n");
+  }
+}
